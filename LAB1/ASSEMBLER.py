d5f3846cbc9bdee62b28ad2133409bcebeb53ae6
arch3n <221007107@aluno.unb.br>
2026-05-01 17:49:09 -0300

    Input Masking and Verification

--- LAB1/ASSEMBLER.py
+++ LAB1/ASSEMBLER.py
@@ -1,7 +1,7 @@
 regis_global = {
 "zero":"x0", "ra":"x1", "sp":"x2", "gp":"x3", "tp":"x4",
  "t0":"x5","t1":"x6", "t2":"x7",
- "s0/fp":"x8","s1":"x9",
+ "s0":"x8","fp":"x8","s1":"x9",
  'a0': 'x10', 'a1': 'x11', 'a2': 'x12',
  'a3': 'x13', 'a4': 'x14', 'a5': 'x15',
  'a6': 'x16', 'a7': 'x17','s2': 'x18', 's3': 'x19',
@@ -53,13 +53,25 @@ inst_global = { #PRIMEIRO: MATEMÁTICA SEM SER IMEDIATA
     },
     "addi":{ #A PARTIR DAQUI MATEMÁTICA IMEDIATA
         "opcode":"0010011",
-        "funct3":"0x0",
-         "funct7":"0x00"
+        "funct3":"0x0"
+    },
+        "andi":{
+        "opcode":"0010011",
+        "funct3":"0x7"
+    },
+        "ori":{
+        "opcode":"0010011",
+        "funct3":"0x6"
+    },
+        "xori":{
+        "opcode":"0010011",
+        "funct3":"0x4"
     }
 }
 
 non_immediate_math_instructs = ["add","sub","and","or","xor","srl","sll"]
-immediate_math_instructs = ["addi"]
+immediate_math_instructs = ["addi","xori","ori","andi"]
+
 def encoder_reg_opcode(reg):
     global inst_global
     global regis_global
@@ -74,7 +86,6 @@ def encoder_reg_opcode(reg):
     return instrucao_out,reg_out,reg[0]
 
 
-
 with open("entrada.asm", "r",encoding="utf-8") as f:
     linhas = f.readlines()
 
@@ -87,34 +98,52 @@ for i in range(len(linhas)):
         aux = "data"
     elif l == ".text":
         aux = "text"
-    else:
+    elif l != "":
         linha_atual = [x.replace(",","") for x in l.split()]
         linha_atual.insert(0,i+1)
         if aux == "text":
             text.append(linha_atual)
         else:
             data.append(linha_atual)
-
+    else:
+        print(f"Linha {i} pulada. Motivo: Linha Vazia")
+    
+# CONVERSIONS HANDLERS
 def reg_to_bin(reg):
     num = int(reg.replace("x", ""))
     return format(num, "05b")
 
-def hex_to_bin(hex,bits):
-    if not hex.isnumeric():
-        hex = hex[2:]
-    valor = int(hex,16)
+def hex_to_bin(valor,bits):
+    if isinstance(valor, str) and valor.startswith("0x"):
+        valor = int(valor, 16)
+    return format(valor, f"0{bits}b")
+
+def int_to_bin(valor, bits):
+    valor = int(valor)
+    if valor < 0: # máscara de 12 bits para casos negativos como -5
+        valor = valor & ((1 << bits) - 1)
     return format(valor, f"0{bits}b")
 
 def not_immediate_math(inst,reg):
     return hex_to_bin(inst["funct7"],7) + reg_to_bin(reg[2])+reg_to_bin(reg[1])+hex_to_bin(inst["funct3"],3)+reg_to_bin(reg[0])+ inst["opcode"]
 def immediate_math(inst,reg):
-    return hex_to_bin(inst["funct7"],7) + hex_to_bin(reg[2],12)+reg_to_bin(reg[1])+hex_to_bin(inst["funct3"],3)+reg_to_bin(reg[0])+ inst["opcode"]
-for i in text:
-    print(i)
-    inst_now,regis_now,instruct_name = encoder_reg_opcode(i[1:])
-    if instruct_name in non_immediate_math_instructs:
-        binario = not_immediate_math(inst_now,regis_now)
-    if instruct_name in immediate_math_instructs:
-        binario = immediate_math(inst_now,regis_now)
-    hexadecimal = format(int(binario, 2), "08X")
-    print(hexadecimal)
+    return int_to_bin(reg[2],12)+reg_to_bin(reg[1])+hex_to_bin(inst["funct3"],3)+reg_to_bin(reg[0])+ inst["opcode"]
+
+
+def TEXT_OUTPUT(text):
+    global inst_global
+    aux = -1
+    for i in text:
+        aux+=1
+        if i[1] not in inst_global:
+            print(f"Linha {i[0]} pulada. Motivo: Instrução não reconhecida.")
+            continue
+        inst_now,regis_now,instruct_name = encoder_reg_opcode(i[1:])
+        if instruct_name in non_immediate_math_instructs:
+            binario = not_immediate_math(inst_now,regis_now)
+        elif instruct_name in immediate_math_instructs:
+            binario = immediate_math(inst_now,regis_now)
+        hexadecimal = format(int(binario, 2), "08X")
+
+        print(f"{format(aux, "08X")} : {hexadecimal} % {i[0]}: {' '.join(i[1:])} %")
+TEXT_OUTPUT(text)
