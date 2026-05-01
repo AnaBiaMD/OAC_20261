a3f1714bd8b9bda89030a5a386f145aab6c978b9
arch3n <221007107@aluno.unb.br>
2026-05-01 16:44:24 -0300

    Full Non Immediate Math Implementation + Start of Immediate Math

--- LAB1/ASSEMBLER.py
+++ LAB1/ASSEMBLER.py
@@ -10,7 +10,7 @@ regis_global = {
  's10': 'x26', 's11': 'x27',
  "t3":"x28","t4":"x29","t5":"x30","t6":"x31"
 }
-inst_global = {
+inst_global = { #PRIMEIRO: MATEMÁTICA SEM SER IMEDIATA
     "add":{
         "opcode":"0110011",
         "funct3":"0x0",
@@ -36,7 +36,30 @@ inst_global = {
         "funct3":"0x4",
          "funct7":"0x00"
     },
+    "sll":{
+        "opcode":"0110011",
+        "funct3":"0x1",
+         "funct7":"0x00"
+    },
+    "srl":{
+        "opcode":"0110011",
+        "funct3":"0x5",
+         "funct7":"0x00"
+    },
+        "slt":{
+        "opcode":"0110011",
+        "funct3":"0x2",
+         "funct7":"0x00"
+    },
+    "addi":{ #A PARTIR DAQUI MATEMÁTICA IMEDIATA
+        "opcode":"0010011",
+        "funct3":"0x0",
+         "funct7":"0x00"
+    }
 }
+
+non_immediate_math_instructs = ["add","sub","and","or","xor","srl","sll"]
+immediate_math_instructs = ["addi"]
 def encoder_reg_opcode(reg):
     global inst_global
     global regis_global
@@ -44,8 +67,11 @@ def encoder_reg_opcode(reg):
     regis = reg[1:]
     reg_out = []
     for i in regis:
-        reg_out.append(regis_global[i])
-    return instrucao_out,reg_out
+        try:
+            reg_out.append(regis_global[i])
+        except:
+            reg_out.append(i)
+    return instrucao_out,reg_out,reg[0]
 
 
 
@@ -56,7 +82,6 @@ data = []
 text = []
 aux = ""
 for i in range(len(linhas)):
-    print(i+1)
     l = linhas[i].strip()
     if l == ".data":
         aux = "data"
@@ -69,24 +94,27 @@ for i in range(len(linhas)):
             text.append(linha_atual)
         else:
             data.append(linha_atual)
-print(text)
-print(data)
 
 def reg_to_bin(reg):
     num = int(reg.replace("x", ""))
     return format(num, "05b")
 
 def hex_to_bin(hex,bits):
-    hex = hex[2:]
+    if not hex.isnumeric():
+        hex = hex[2:]
     valor = int(hex,16)
     return format(valor, f"0{bits}b")
 
 def not_immediate_math(inst,reg):
     return hex_to_bin(inst["funct7"],7) + reg_to_bin(reg[2])+reg_to_bin(reg[1])+hex_to_bin(inst["funct3"],3)+reg_to_bin(reg[0])+ inst["opcode"]
-    
+def immediate_math(inst,reg):
+    return hex_to_bin(inst["funct7"],7) + hex_to_bin(reg[2],12)+reg_to_bin(reg[1])+hex_to_bin(inst["funct3"],3)+reg_to_bin(reg[0])+ inst["opcode"]
 for i in text:
-    inst_now,regis_now = encoder_reg_opcode(i[1:])
-    binario = not_immediate_math(inst_now,regis_now)
-
+    print(i)
+    inst_now,regis_now,instruct_name = encoder_reg_opcode(i[1:])
+    if instruct_name in non_immediate_math_instructs:
+        binario = not_immediate_math(inst_now,regis_now)
+    if instruct_name in immediate_math_instructs:
+        binario = immediate_math(inst_now,regis_now)
     hexadecimal = format(int(binario, 2), "08X")
     print(hexadecimal)
--- LAB1/entrada.asm
+++ LAB1/entrada.asm
@@ -1,4 +1,6 @@
 .text
+addi t2, zero, 4
+sll t1, t1, t2
 add t3, t2, t1
 sub t3, t1, t3
 and t4, t1, t2
