1fb182d7c04b9d765da3801aba095a2452439729
Pedrodouradobraga-lab <pedrodouradobraga@gmail.com>
2026-05-02 20:25:42 -0300

    Entrada com todas as instruções da checklist
    Refactor assembly logic for better clarity and functionality.

--- LAB1/entrada.asm
+++ LAB1/entrada.asm
@@ -10,6 +10,11 @@ inicio: auipc, t0, 0
 jalr t6, 8(t0)
 lui t1, 0x1001
 addi t2, zero, 4
+add t6, t2, zero
+beq t2, t6, f
+g: sub t6, t6, t6
+f: add t6, t6, t2
+bne t2, t6, g
 sll t1, t1, t2
 lw t2, 0(t1)
 sw t2, 32(t1)
@@ -18,6 +23,10 @@ sub t3, t1, t3
 and t4, t1, t2
 or t4, t1, t2
 xor t4, t1, t2
+slt t6, t2, t6
+andi t6, t2, 1
+ori t6, t2, 0
+xori t6, t2, 1
 jal rotulo
 beq t1, t2, rotulo
 lhu t5, 32(t1)
